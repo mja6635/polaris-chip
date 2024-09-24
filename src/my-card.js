18bb0ec9a882d22ceb882ed3bee4761e5ada87f0
mja6635 <mja6635@psu.edu>
2024-09-24 12:49:34 -0400

    Update my-card.js

--- src/my-card.js
+++ src/my-card.js
@@ -48,8 +48,7 @@ export class MyCard extends LitElement {
       /* Paragraph (description) styles inside the card */
       .card p {
         font-size: 16px;
-        background-color: black;
-        color: gold;
+        color: black;
         margin-bottom: 12px;
       }
 
@@ -111,6 +110,50 @@ export class MyCard extends LitElement {
             <h1 class="card-title"><i>This is my cat</i></h1>
             <p>Not only is this not my cat but this cat also hates codepen</p>
             <a href="https://hax.psu.edu" class="awesome-button">Details</a>
+          </div>
+        </section>
+      </div>
+
+      <div id="cardlist"> 
+        <section class="card">
+          <img class="card-image" src="https://deadline.com/wp-content/uploads/2022/08/Lebron-James.jpg?w=1000" alt="Card image" />
+          <div class="card-info">
+            <h1 class="card-title"><i>My Glorious King</i></h1>
+            <p>If he tells me to jump, I say "How High"</p>
+            <a href="https://allpoetry.com/poem/17718975-My-glorious-king--by-Lebronjames" class="awesome-button">Please Read</a>
+          </div>
+        </section>
+      </div>
+
+      <div id="cardlist"> 
+        <section class="card">
+          <img class="card-image" src="https://helios-i.mashable.com/imagery/articles/00crJ2ChE95JN4zloBxW5Sn/hero-image.fill.size_1200x675.v1719433368.png" alt="Card image" />
+          <div class="card-info">
+            <h1 class="card-title"><i>Is this the greatest moment in history?</i></h1>
+            <p>Why does she have a podcast</p>
+            <a href="https://www.youtube.com/@TalkTuahPod" class="awesome-button">Listen to it here</a>
+          </div>
+        </section>
+      </div>
+
+      <div id="cardlist"> 
+        <section class="card">
+          <img class="card-image" src="https://www.nytimes.com/games-assets/v2/assets/wordle/wordle_og_1200x630.png" alt="Card image" />
+          <div class="card-info">
+            <h1 class="card-title"><i>I play wordle in almost all of my classes</i></h1>
+            <p>Only one class tho because it is once a day</p>
+            <a href="https://www.nytimes.com/games/wordle/index.html" class="awesome-button">Play it here</a>
+          </div>
+        </section>
+      </div>
+
+      <div id="cardlist"> 
+        <section class="card">
+          <img class="card-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Blank_US_Map%2C_Mainland_with_no_States.svg/2560px-Blank_US_Map%2C_Mainland_with_no_States.svg.png" alt="Card image" />
+          <div class="card-info">
+            <h1 class="card-title"><i>You have a serious gamer chair if you can get a 100% on this</i></h1>
+            <p>Point to all states no boarders</p>
+            <a href="https://www.sporcle.com/games/g/states" class="awesome-button">Good luck if you do it!</a>
           </div>
         </section>
       </div>
